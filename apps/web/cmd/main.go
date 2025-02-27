53225aa8b97ceb4684257c029b16fe4720305546
Rafal Pienkowski <rafal.pienkowski@m-kopa.com>
2025-02-27 20:59:35 +0100

    Add natural key

--- apps/web/cmd/main.go
+++ apps/web/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"io"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -48,25 +49,31 @@ func newPage() Page {
 }
 
 type Order struct {
-	Tea    int `json:"tea"`
-	Coffee int `json:"coffee"`
-	Cake   int `json:"cake"`
-	Soda   int `json:"soda"`
+	Tea    int    `json:"tea"`
+	Coffee int    `json:"coffee"`
+	Cake   int    `json:"cake"`
+	Soda   int    `json:"soda"`
+	Name   string `json:"name"`
 	Errors []string
 }
 
-func parseOrder(tea string, coffee string, cake string, soda string) Order {
+func parseOrder(tea string, coffee string, cake string, soda string, name string) Order {
 
 	order := Order{
 		Tea:    parseInt(tea),
 		Coffee: parseInt(coffee),
 		Cake:   parseInt(cake),
 		Soda:   parseInt(soda),
+		Name:   name,
 	}
 
-    if order.Tea == 0 && order.Coffee == 0 && order.Cake == 0 && order.Soda == 0 {
-        order.Errors = append(order.Errors, "Please order something")
-    }
+	if order.Tea == 0 && order.Coffee == 0 && order.Cake == 0 && order.Soda == 0 {
+		order.Errors = append(order.Errors, "Please order something")
+	}
+
+	if len(order.Name) < 3 {
+		order.Errors = append(order.Errors, "Please provide 3 letters name")
+	}
 
 	return order
 }
@@ -87,7 +94,6 @@ func main() {
 	e.Static("/images", "images")
 	e.Static("/css", "css")
 	page := newPage()
-	orderId := 0
 
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "index", page)
@@ -100,23 +106,25 @@ func main() {
 			c.FormValue("coffee"),
 			c.FormValue("cake"),
 			c.FormValue("soda"),
+			c.FormValue("name"),
 		)
 		e.Logger.Debugf("Received order: %+v", order)
 
-        if order.Errors != nil {
-            form := newFormData()
-            form.Values["tea"] = strconv.Itoa(order.Tea)
-            form.Values["caffee"] = strconv.Itoa(order.Coffee)
-            form.Values["cake"] = strconv.Itoa(order.Cake)
-            form.Values["soda"] = strconv.Itoa(order.Soda)
-            form.Errors = order.Errors
-            return c.Render(422, "form", form)
-        }
+		if order.Errors != nil {
+			form := newFormData()
+			form.Values["tea"] = strconv.Itoa(order.Tea)
+			form.Values["caffee"] = strconv.Itoa(order.Coffee)
+			form.Values["cake"] = strconv.Itoa(order.Cake)
+			form.Values["soda"] = strconv.Itoa(order.Soda)
+			form.Values["name"] = order.Name
+			form.Errors = order.Errors
+			return c.Render(422, "form", form)
+		}
 
 		c.Render(200, "form", newFormData())
 
-		orderId += 1
-		return c.Render(200, "confirmation", fmt.Sprintf("Your order id is: %d", orderId))
+		orderId := order.Name + "-" + time.Now().Format("2006-01-02 15:04:05.350")
+		return c.Render(200, "confirmation", fmt.Sprintf("Your order id is: %v", orderId))
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
